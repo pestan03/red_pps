209fe3a28bcb2d41af01c721a4b05b24ccbaea9e
pestan03 <pabloestan17@gmail.com>
2024-05-16 12:32:07 +0200

    Update pruebasRegistro.php

--- html/tests/pruebasRegistro.php
+++ html/tests/pruebasRegistro.php
@@ -1,6 +1,6 @@
 <?php
 use PHPUnit\Framework\TestCase;
-require_once("/home/runner/work/red_pps/red_pps/html/php/registro.php");
+require_once("/home/runner/work/red_pps/red_pps/html/php/");
 class PruebasRegistro extends TestCase
 {
     public function testValidarContrasena()
