7b0b2d4fe10feede1214d3a202783fbbad3c2e41
pestan03 <pabloestan17@gmail.com>
2024-05-16 19:16:07 +0200

    Update pruebasRegistro.php

--- html/tests/pruebasRegistro.php
+++ html/tests/pruebasRegistro.php
@@ -1,6 +1,7 @@
 <?php
 use PHPUnit\Framework\TestCase;
-require_once("/home/runner/work/red_pps/red_pps/html/php/funciones.php");
+include("../php/funciones.php"); // Asegúrate de que esta ruta es correcta y apunta al archivo que contiene la clase Funciones
+
 class PruebasRegistro extends TestCase
 {
     public function testValidarContrasena()
@@ -8,35 +9,34 @@ class PruebasRegistro extends TestCase
         // Pruebas para la función validar_contraseña
         
         // Caso: Contraseña válida
-        $this->assertTrue(validar_contraseña("Contraseña1#"));
+        $this->assertTrue(Funciones::validar_contraseña("Contraseña1#"));
         
         // Caso: Contraseña demasiado corta
-        $this->assertFalse(validar_contraseña("short1#"));
+        $this->assertFalse(Funciones::validar_contraseña("short1#"));
         
         // Caso: Contraseña sin mayúsculas
-        $this->assertFalse(validar_contraseña("minuscula1#"));
+        $this->assertFalse(Funciones::validar_contraseña("minuscula1#"));
         
         // Caso: Contraseña sin minúsculas
-        $this->assertFalse(validar_contraseña("MAYUSCULA1#"));
+        $this->assertFalse(Funciones::validar_contraseña("MAYUSCULA1#"));
         
         // Caso: Contraseña sin dígitos
-        $this->assertFalse(validar_contraseña("SoloLetras#"));
+        $this->assertFalse(Funciones::validar_contraseña("SoloLetras#"));
         
         // Caso: Contraseña sin caracteres especiales
-        $this->assertFalse(validar_contraseña("MissingSpecialChar1"));
+        $this->assertFalse(Funciones::validar_contraseña("MissingSpecialChar1"));
     }
     
     public function testComprobarDniLetra()
     {
-
         // Caso: DNI válido
-        $this->assertTrue(comprobar_dni_letra("12345678Z"));
+        $this->assertTrue(Funciones::comprobar_dni_letra("12345678Z"));
 
         // Caso: DNI con letra incorrecta
-        $this->assertFalse(comprobar_dni_letra("12345678A"));
+        $this->assertFalse(Funciones::comprobar_dni_letra("12345678A"));
 
         // Caso: DNI con letra correcta
-        $this->assertTrue(comprobar_dni_letra("87654321R"));
+        $this->assertTrue(Funciones::comprobar_dni_letra("87654321R"));
     }
 }
 ?>
