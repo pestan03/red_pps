0729574f2d8a1a8451225aa57d5c2e9528158bc2
pestan03 <pabloestan17@gmail.com>
2024-05-16 19:17:05 +0200

    Update pruebasRegistro.php

--- html/tests/pruebasRegistro.php
+++ html/tests/pruebasRegistro.php
@@ -1,6 +1,6 @@
 <?php
 use PHPUnit\Framework\TestCase;
-include("../php/funciones.php"); // Asegúrate de que esta ruta es correcta y apunta al archivo que contiene la clase Funciones
+include("/home/runner/work/red_pps/red_pps/html/php/funciones.php"); // Asegúrate de que esta ruta es correcta y apunta al archivo que contiene la clase Funciones
 
 class PruebasRegistro extends TestCase
 {
