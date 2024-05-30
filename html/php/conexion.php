8789102b019e06ee47504f3cc293a467f1b0401d
pestan03 <pabloestan17@gmail.com>
2024-05-30 06:52:55 -0400

    f

--- html/php/conexion.php
+++ html/php/conexion.php
@@ -1,19 +1,47 @@
 <?php
-    $servername = "localhost";
-    $username = "id22220192_pablo";
-    $password = "pabloESP03@"; // Sin contraseña
-    $database = "id22220192_red"; 
-    // $servername = "db";
-    // $username = "root";
-    // $password = "root_password"; // Sin contraseña
-    // $database = "red"; 
-    $port= 3306;    
-    try {
-        $conn = new PDO("mysql:host=$servername;port=$port;dbname=$database", $username, $password);
-        // Establecer el modo de error PDO a excepción
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(PDOException $e) {
-        // Manejo de errores de conexión
-        error_log("Error de conexión: " . $e->getMessage());
-        die("Error de conexión a la base de datos. Por favor, inténtalo de nuevo más tarde.");
+function loadEnv($path)
+{
+    if (!file_exists($path)) {
+        throw new Exception("El archivo .env no existe.");
     }
+
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) {
+            continue; // Ignorar comentarios
+        }
+        
+        list($name, $value) = explode('=', $line, 2);
+        $name = trim($name);
+        $value = trim($value);
+
+        if (!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) {
+            putenv(sprintf('%s=%s', $name, $value));
+            $_ENV[$name] = $value;
+            $_SERVER[$name] = $value;
+        }
+    }
+}
+
+// Cargar las variables de entorno desde el archivo .env
+loadEnv(__DIR__ . '/.env');
+
+// Obtener las variables de entorno
+$servername = getenv('DB_SERVER');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_NAME');
+$port = getenv('DB_PORT');
+
+try {
+    // Crear una instancia de PDO para la conexión a la base de datos
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$database", $username, $password);
+    // Establecer el modo de error de PDO a excepción
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión exitosa";
+} catch(PDOException $e) {
+    // Manejo de errores de conexión
+    error_log("Error de conexión: " . $e->getMessage());
+    die("Error de conexión a la base de datos. Por favor, inténtalo de nuevo más tarde.");
+}
+?>
