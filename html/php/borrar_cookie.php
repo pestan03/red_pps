74928f9475c054c22487d19643b5182b9ba8b3e3
pestan03 <pabloestan17@gmail.com>
2024-05-30 06:36:11 -0400

    Update borrar_cookie.php

--- html/php/borrar_cookie.php
+++ html/php/borrar_cookie.php
@@ -1,8 +1,6 @@
 <?php
 // Establece el valor de la cookie a uno vacío
 setcookie('cookie_session', '', time() - 3600, '/', '', false, true); // Establece el tiempo de expiración en el pasado
-setcookie('session_token', '', time() - 3600, '/', '', false, true); // 'httpOnly' set to true
-
 // Responde con un estado HTTP 200 para indicar que la operación se realizó correctamente
 http_response_code(200);
 
