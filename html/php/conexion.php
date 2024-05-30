fd29e41439c3fabfdc64c4ebdbb7749829eda1f5
pestan03 <pabloestan17@gmail.com>
2024-05-30 06:24:10 -0400

    .env

--- html/php/conexion.php
+++ html/php/conexion.php
@@ -1,19 +1,17 @@
 <?php
-    $servername = "localhost";
-    $username = "id22220192_pablo";
-    $password = "pabloESP03@"; // Sin contraseña
-    $database = "id22220192_red"; 
-    // $servername = "db";
-    // $username = "root";
-    // $password = "root_password"; // Sin contraseña
-    // $database = "red"; 
-    $port= 3306;    
-    try {
-        $conn = new PDO("mysql:host=$servername;port=$port;dbname=$database", $username, $password);
-        // Establecer el modo de error PDO a excepción
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(PDOException $e) {
-        // Manejo de errores de conexión
-        error_log("Error de conexión: " . $e->getMessage());
-        die("Error de conexión a la base de datos. Por favor, inténtalo de nuevo más tarde.");
-    }
+$servername = getenv('DB_SERVER');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_NAME');
+$port = 3306; // Suponiendo un puerto predeterminado
+
+try {
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$database", $username, $password);
+    // Establecer el modo de error PDO a excepción
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    // Manejo de errores de conexión
+    error_log("Error de conexión: " . $e->getMessage());
+    die("Error de conexión a la base de datos. Por favor, inténtalo de nuevo más tarde.");
+}
+?>
